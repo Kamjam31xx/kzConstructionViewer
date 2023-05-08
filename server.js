e0e8d279d688a20aa10c33d46cbd7f4e4cda420b
Kamjam21xx <kamjam31xx@gmail.com>
2023-05-08 01:47:51 -0400

    testing

--- server.js
+++ server.js
@@ -15,29 +15,13 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const aboutRouter = require('./routes/about');
-const usersRouter = require('./routes/users');
-//app.use('/about', aboutRouter);
-//app.use('/users', usersRouter);
+
 app.use(logger);
 
-// public static over-rides below .get() since its above this
-/*
-app.get('/', (request, response, next) => {
 
-    //response.send("Hi from root directory");
-
-    //response.status(status).send("hi");
-    //response.status(500).json({ message: "Error" });
-    //response.download("filepath");
-
-    response.render('./views/index', { text: "world" });
-});
-*/
-
-const PORT = 8080;
+const PORT = 9090;
 //app.listen(8080, "0.0.0.0");
-app.listen(8080, () => {
+app.listen(9090, () => {
     console.log(`node server started... listening on port <${PORT}>`);
 });
 
