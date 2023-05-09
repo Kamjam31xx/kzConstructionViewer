3d83d54b719095571caa550b3940db909eae2e11
Kamjam21xx <kamjam31xx@gmail.com>
2023-05-09 14:38:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger);
-//app.use(httpRedirect);
 app.use(express.static("public"));
+app.use(httpRedirect);
 
 
 
